de4f87ea1ebb0582c04cf1a4e250bd7001626f93
Evan Pavley <epavley@sva.edu>
2018-11-12 17:50:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var a = []
+
+function addElementToBeginningOfArray(a, b){
+  return [b, ...a];
+}
+
+function destructivelyAddElementToBeginningOfArray(a, b){
+  a.unshift(b);
+  return a;
+}
+
+function addElementToEndOfArray(a, b){
+  return [...a, b];
+}
+ 
+function destructivelyAddElementToEndOfArray(a, b){
+  a.push(b);
+  return a;
+}
